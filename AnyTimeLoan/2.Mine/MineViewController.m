2f505b54cef96a7a21feb2e5ce341cbd8e3c7f30
DragonetZ <zxl3514@126.com>
2017-12-09 15:20:24 +0800

    我的中点击动画

--- AnyTimeLoan/2.Mine/MineViewController.m
+++ AnyTimeLoan/2.Mine/MineViewController.m
@@ -61,6 +61,7 @@
 
 #pragma mark - UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
     self.selectedIndexPath = indexPath;
     [self performSegueWithIdentifier:@"showMineDetail" sender:self];
 }
