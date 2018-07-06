536955a9719492266d3a042ac1fa64175b9bf123
DragonetZ <zxl3514@126.com>
2018-07-06 15:46:51 +0800

    test

--- AnyTimeLoan/AppDelegate.m
+++ AnyTimeLoan/AppDelegate.m
@@ -16,7 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
     
     [UITabBar appearance].tintColor = CommonColor;
     [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
